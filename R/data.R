6e80cba78ff60def06dd3380bf3ac67db8d23d96
DavidPayares <43214524+DavidPayares@users.noreply.github.com>
2023-05-22 11:56:39 +0200

    Innit commit

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' MRI scans
+#'
+#' A set of folders containing the MRI scans for four
+#' neurodegenerative patients. Each folder contains at
+#' least one MRI scan (usually a T1-weighted sequence).
+#' These sample data is used to reproduce the examples
+#' of the NeuroNorm package.
+#'
+#' @details The MRI scans comprise 3-dimensional images
+#' of the brain structure for four patients with neurodegenerative
+#' disorders.
+#'
+#' @format Folders containing MRI scans in NiFTI format.
